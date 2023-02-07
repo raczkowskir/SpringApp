f46fd44e4540779d21662ec71f9e14f08ad5678d
Rafal Raczkowski <rafal.raczkowski@nokia.com>
2023-02-07 16:50:52 +0100

    MVN changes

--- src/main/java/com/hackerman/springapp/SpringAppApplication.java
+++ src/main/java/com/hackerman/springapp/SpringAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAppApplication.class, args);
+    }
 
 }
