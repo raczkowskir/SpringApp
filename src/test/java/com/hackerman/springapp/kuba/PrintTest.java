65faf64248e5efe4f9a9328f8d4e615bcb1b1ea2
keeeper85 <122378010+keeeper85@users.noreply.github.com>
2023-03-07 09:47:13 +0100

    Test commit

---
+++ src/test/java/com/hackerman/springapp/kuba/PrintTest.java
@@ -0,0 +1,20 @@
+package com.hackerman.springapp.kuba;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrintTest {
+
+    @Test
+    void printName() {
+
+        //given
+
+
+        //when
+
+
+        //then
+    }
+}
